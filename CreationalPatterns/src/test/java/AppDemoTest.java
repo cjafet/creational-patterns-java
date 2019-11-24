0bd5291427e5f8616aff91883a58c719f7a3e17b
Carlos Jafet Neto <cjafet07@gmail.com>
2019-11-24 20:45:40 -0200

    Test jenkins integration

---
+++ CreationalPatterns/src/test/java/AppDemoTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppDemoTest {
+	
+	@Test
+	public void test( ) {
+		assertEquals(true, true);
+	}
+
+}
