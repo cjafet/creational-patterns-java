2397061f348ec1fd492f773ac707852f93815cbc
Carlos Jafet Neto <cjafet07@gmail.com>
2019-11-25 22:11:05 -0200

    Add package

---
+++ CreationalPatterns/src/test/java/com/cjafet/AppDemoTest.java
@@ -0,0 +1,14 @@
+package test.java.com.cjafet;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppDemoTest {
+	
+	@Test
+	public void test( ) {
+		assertEquals(true, true);
+	}
+
+}
